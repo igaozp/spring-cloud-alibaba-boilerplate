5c3c0f0126f8643216f975b7a890f7b3ebb68b50
igaozp <igaozp@hotmail.com>
2020-12-13 22:03:07 +0800

    feat: 模拟测试配置中心动态配置

--- consumer/src/main/java/xyz/andornot/TestController.java
+++ consumer/src/main/java/xyz/andornot/TestController.java
@@ -1,8 +1,11 @@
 package xyz.andornot;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +19,7 @@ import javax.annotation.Resource;
  * @author igaozp
  * @since 2020/12/13
  */
+@RefreshScope
 @RestController
 public class TestController {
     @Bean
@@ -23,15 +27,23 @@ public class TestController {
         return new RestTemplate();
     }
 
+    @Lazy
     @Resource
     private RestTemplate restTemplate;
     @Resource
     private LoadBalancerClient loadBalancerClient;
+    @Value("${testConfig:false}")
+    private boolean testConfig;
 
     @GetMapping("/echo/{str}")
     public String echo(@PathVariable String str) {
         ServiceInstance serviceInstance = loadBalancerClient.choose("provider");
         String url = String.format("http://%s:%s/echo/%s", serviceInstance.getHost(), serviceInstance.getPort(), str);
         return restTemplate.getForObject(url, String.class);
+    }
+
+    @GetMapping("config")
+    public boolean config() {
+        return testConfig;
     }
 }
