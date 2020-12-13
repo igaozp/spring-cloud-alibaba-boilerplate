13a7de9a064a9a60d3cfce3a7fab1565daa4ec61
igaozp <igaozp@hotmail.com>
2020-12-13 20:43:25 +0800

    feat: 初步添加服务提供者

---
+++ provider/src/main/java/xyz/andornot/ProviderApplication.java
@@ -0,0 +1,19 @@
+package xyz.andornot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * service provider application
+ *
+ * @author igaozp
+ * @since 2020/12/13
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ProviderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProviderApplication.class, args);
+    }
+}
